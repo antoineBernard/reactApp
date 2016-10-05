29477526d3e178a50902a45021e2419b2931e012
Antoine BERNARD <abernard@octo.com>
2016-10-05 11:46:38 +0200

    add character count

--- src/TweetBox.js
+++ src/TweetBox.js
@@ -17,6 +17,7 @@ var TweetBox = React.createClass({
         <textarea className="form-control"
                   onChange={this.handleChange}></textarea>
         <br/>
+        <span className="pull-left"> {140 - this.state.text.length} </span>
         <button className="btn btn-primary pull-right"
                 disabled={this.state.text.length === 0}>Tweet</button>
       </div>
