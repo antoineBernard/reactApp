e3de4757727878fc6ab8401798e4b38823db9111
Antoine BERNARD <abernard@octo.com>
2016-10-05 15:21:54 +0200

    add overflow gestion

--- src/TweetBox.js
+++ src/TweetBox.js
@@ -11,9 +11,31 @@ var TweetBox = React.createClass({
     this.setState({ text: event.target.value });
   },
 
+  overflowAlert: function() {
+    if (this.remainingCharacters() < 0) {
+      var beforeOverflowText = this.state.text.substring(140 - 10, 140);
+      var overflowText       = this.state.text.substring(140);
+
+      return (
+        <div className="alert alert-warning">
+          <strong>Oops! Too Long:</strong>
+          &nbsp;...{beforeOverflowText}
+          <strong className="bg-danger">{overflowText}</strong>
+        </div>
+      );
+    } else {
+      return "";
+      }
+  },
+
+  remainingCharacters: function() {
+    return 140 - this.state.text.length
+  },
+
   render: function() {
     return (
       <div className="well clearfix">
+        { this.overflowAlert() }
         <textarea className="form-control"
                   onChange={this.handleChange}></textarea>
         <br/>
